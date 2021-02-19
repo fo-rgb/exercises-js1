0a8d709fdc7c3c7b231fa3fcccb10b8b5fba32de
fode moussa <fodemakale18@gmial.com>
2021-02-19 18:30:54 +0100

    soy nuevo commit java

---
+++ week-1/Homework/L-functions-nested/exercise2.js
@@ -0,0 +1,22 @@
+const numberOfStudents= 65;
+const numberOfMentors= 35;
+const numtotal= numberOfStudents + numberOfMentors;
+
+console.log("Total",numtotal);
+
+
+console.log("porcentage Students " + numberOfStudents +  "%");
+console.log("porcentage Mentors "  +  numberOfMentors + "%");
+console.log("porcentage total " + numtotal + "%");
+
+
+const añonaci=2000;
+const añoactu=2021;
+
+const resulta= añonaci - añoactu;
+const añonacito= "he nacido en " + añonaci;
+const añoactual="y " + añoactu +" es el año actual";
+const ano="años";
+
+console.log(añonacito,añoactual);
+console.log("Entonces tengo", resulta, ano);
